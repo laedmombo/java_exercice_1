930395256e4bccd6210f59720d9e39ff535ccb71
laedmombo <mombomombo@et.esiea.fr>
2021-11-29 15:45:59 +0100

    third commit

--- src/Launcher.java
+++ src/Launcher.java
@@ -2,10 +2,18 @@ import java.util.Scanner;
 public class Launcher {
     public static void main(String[] args){
         System.out.println(" Bienvenue dans le fichier Launcher ");
-        Scanner scanner = new Scanner( System.in );
-        String entrer = scanner.nextLine();
-        if(!entrer.equals( "quit" )){
-            System.out.println("Unknown command");
+        try ( Scanner scanner = new Scanner( System.in ) ) {
+            while( true ) {
+                System.out.print( "Entrer une commande : " );
+                String entrer = scanner.nextLine();
+                if ( entrer.equals( "quit" ) ) {
+                    break;
+                }
+                else {
+                    System.out.println("Unknown command");
+                }
+                }
+            }
         }
     }
-}
+
