06e854a3096611e5cf21dbfae6aed9d3b8325d74
laedmombo <mombomombo@et.esiea.fr>
2021-12-02 14:22:13 +0100

    final

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,4 +1,8 @@
 import java.util.Scanner;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.io.IOException;
 public class Launcher {
     public static void main(String[] args){
         System.out.println(" Bienvenue dans le fichier Launcher ");
@@ -11,6 +15,27 @@ public class Launcher {
                     System.out.println("Entrer un entier n : ");
                     Integer nbr = scanner.nextInt();
                     System.out.println("La suite de Fibonacci d'ordre "+nbr+" vaut "+ Fib(nbr));
+                    break;
+                }
+                else if (entrer.equals("freq")){
+                    System.out.println("Entrer le chemin du fichier à lire : ");
+                    String path = scanner.nextLine();
+                    Path filePath = Paths.get(path);
+                    try
+                    {
+                        String content = Files.readString(filePath);
+                        String result = content.replaceAll("\\p{Punct}", " ");
+                        String[] TabResult = result.split(" ");
+                        List<String> al = (ArrayList<String>)toArrayList(TabResult);
+                        for (int i = 0; i < TabResult.length; i++) {
+                            System.out.println(TabResult[i]);
+                        }
+                        //System.out.println(TabResult);
+                    }
+                    catch (IOException e)
+                    {
+                        System.err.println("Unreadable file: " + e.getClass() +"  "+ e.getCause());
+                    }
                     break;
                 }
                 else{
